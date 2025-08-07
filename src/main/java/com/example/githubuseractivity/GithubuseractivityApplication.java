86d4fcef917be68a0715a34f25580ac53effb451
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-06 20:35:33 -0600

    Fixed RestTemplate

--- src/main/java/com/example/githubuseractivity/GithubuseractivityApplication.java
+++ src/main/java/com/example/githubuseractivity/GithubuseractivityApplication.java
@@ -2,6 +2,8 @@ package com.example.githubuseractivity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GithubuseractivityApplication {
@@ -10,4 +12,8 @@ public class GithubuseractivityApplication {
 		SpringApplication.run(GithubuseractivityApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate () {
+		return new RestTemplate ();
+	}
 }
