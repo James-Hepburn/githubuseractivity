c392bacbf516ba67457f3be3c8e4f3d38cb79eb2
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-06 19:45:24 -0600

    Initial commit

---
+++ src/main/java/com/example/githubuseractivity/GithubuseractivityApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubuseractivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubuseractivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubuseractivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubuseractivity/GithubuseractivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubuseractivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubuseractivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
